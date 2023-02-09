7ace330a96071d625a15850805f82c26fc4a9c5d
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2023-02-09 11:54:56 +0530

    ???

--- amogus.py
+++ amogus.py
@@ -1,23 +1,8 @@
+import json
 import requests
-import os
 import argparse
 
-# Configure this if the search results are not enough for you
-SEARCH_LIMIT = 25
-
-# API URL
-API_URL = 'https://itunes.apple.com/'
-
-# Logo
-LOGO = r"""
-  __ _ _ __ ___   ___   __ _ _   _ ___
- / _` | '_ ` _ \ / _ \ / _` | | | / __|
-( (_| | | | | | | (_) | (_| | |_| \__ \
- \__,_|_| |_| |_|\___/ \__, |\__,_|___/
-repository:             __/ |Ver: 1.0.0
-github.com/R3AP3/amogus|___/           
-"""
-
+API_URL = 'https://itunes.apple.com/'  # API URL
 
 def download_artwork(url, filename):
     r = requests.get(url)
@@ -25,7 +10,7 @@ def download_artwork(url, filename):
         ext = r.headers['Content-Type'].split('/')[1]
         if ext == 'jpeg':
             ext = 'jpg'
-        with open(f'cover/{filename}.{ext}', 'wb') as f:
+        with open(f'{filename}.{ext}', 'wb') as f:
             f.write(r.content)
     else:
         print('Error downloading artwork [' + str(r.status_code) + ']')
@@ -36,24 +21,14 @@ def request(request_action, params):
     return r.json()
 
 
-def search(search_input, search_type):
-    params = {
-        'term': search_input,
-        'entity': search_type,
-        'limit': SEARCH_LIMIT
-    }
-    return request('search', params)
-
-
 def lookup(lookup_input, region):
-    params = {
+    lookup_info = request('lookup', {
         'id': lookup_input,
         'country': region,
         'entity': 'album',
         'limit': 200,
         'sort': 'recent'
-    }
-    lookup_info = request('lookup', params)
+    })
     for result in lookup_info['results']:
         if result['wrapperType'] == 'collection':
             print(f"{result['collectionName']} [{result['collectionId']}]")
@@ -67,60 +42,42 @@ def lookup(lookup_input, region):
 def url_parse(url):
     split_url = url.split('/')
     if split_url[3] == 'artist' or split_url[3] == 'album':
-        return split_url[-1], 'us', split_url[3]
+        return split_url[-1], 'us'
     else:
-        return split_url[-1], split_url[3], split_url[4]
+        return split_url[-1], split_url[3]
 
 
 def main(args):
     if args.search:
-        if args.artist_search:
-            shit = ['musicArtist', 'artistId', 'artistName']
-        else:
-            shit = ['album', 'collectionId', 'collectionName']
-        results = search(args.input, shit[0])['results']
-        for i in range(len(results)):
-            print(f"[{i + 1}] {results[i][shit[2]]} [{results[i][shit[1]]}]")
+        shit = ['musicArtist', 'artistId', 'artistName'] if args.artist_search else ['album', 'collectionId', 'collectionName']
+        results = request('search', {
+            'term': args.input, 'entity': shit[0], 'limit': args.limit
+        })['results']
+        for n, item in enumerate(results, start=1):
+            print(json.dumps(item, indent=2))
+            print(f"[{n}] {item[shit[2]]} [{item[shit[1]]}]")
         choice = input("\nChoice: ")
         if choice.isdigit():
-            choice = int(choice) - 1
-            lookup(results[choice][shit[1]], args.region_code)
+            lookup(results[int(choice) - 1][shit[1]], args.region_code)
     else:
-        adam_id, region, lookup_type = url_parse(args.input)
-        lookup(adam_id, region)
+        lookup(*url_parse(args.input))
 
 
 if __name__ == "__main__":
-    print(LOGO)
-    if not os.path.exists("cover"):
-        os.makedirs("cover")
     parser = argparse.ArgumentParser(
-        description="Downloads Album Covers as their Source File from the Itunes Search API"
-    )
-    parser.add_argument(
-        "-s", "--search",
-        dest='search',
-        help="Search for Albums",
-        action="store_true"
-    )
-    parser.add_argument(
-        "-a", "--artist",
-        dest='artist_search',
+        description="Downloads Album Covers as their Source File from the Itunes Search API")
+    parser.add_argument("-s", "--search",
+        dest='search', help="Search for Albums", action="store_true")
+    parser.add_argument("-a", "--artist",
         help="Search for Artist (use in combination with --search)",
-        action="store_true"
-    )
-    parser.add_argument(
-        "-r", "--region",
-        type=str, dest='region_code',
+        dest='artist_search', action="store_true")
+    parser.add_argument("-r", "--region",
         help="Set Region Code [Default: 'us'] (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)",
-        default="us",
-        metavar="REGION_CODE"
-    )
-    parser.add_argument(
-        "input",
-        type=str,
-        help="Apple Music URL or Search Query",
-        metavar="INPUT"
-    )
+        type=str, dest='region_code', default="us", metavar="REGION_CODE")
+    parser.add_argument("-l", "--limit",
+        help="Configure this if the search results are not enough for you",
+        type=int, default=5, metavar="NUM")
+    parser.add_argument("input",
+        help="Apple Music URL or Search Query", type=str, metavar="INPUT")
+
     main(parser.parse_args())
-    print("Done")
